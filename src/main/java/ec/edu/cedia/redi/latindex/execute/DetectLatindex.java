903cd2375c9e665130c4c6e0b068f4116732bd62
José Ortiz <jeortizvivar@gmail.com>
2018-06-08 12:24:15 -0500

    Fix HTTP exceptions

--- src/main/java/ec/edu/cedia/redi/latindex/execute/DetectLatindex.java
+++ src/main/java/ec/edu/cedia/redi/latindex/execute/DetectLatindex.java
@@ -105,7 +105,7 @@ public class DetectLatindex {
         } while (true);
     }
 
-    private static int last = 170800;
+    private static int last = 182280;
 
     public static void main2(String[] args) throws RepositoryException, Exception {
         try (RediRepository r = RediRepository.getInstance()) {
